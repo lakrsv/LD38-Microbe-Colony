8be1d6022098257d9e2c5152cb102de404752a7c
lakrsv <lakrsv@gmail.com>
2017-04-22 12:38:26 +0100

    Initial commit

---
+++ Assets/Scripts/Tile/Old Reference/TileMap.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tile.Old
+{
+    public class TileMap : MonoBehaviour
+    {
+        private const int _gridWidth = 10;
+        private const int _gridHeight = 10;
+        private int[,] _tileGrid = new int[_gridWidth, _gridHeight];
+
+        private float _xModY = 0.75f;
+        private float _yModX = -0.433f;
+        private float _xIncrement = 0.75f;
+        private float _yIncrement = 0.4333f;
+
+        // Use this for initialization
+        void Start()
+        {
+            ResetGrid();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void PopulateGrid(GameObject tileObj)
+        {
+            for (int i = 0; i < _gridWidth; i++)
+                for (int k = 0; k < _gridHeight; k++)
+                    PlaceTile(i, k, tileObj);
+        }
+
+        public bool PlaceTile(int x, int y, GameObject tilePrefab)
+        {
+            if (_tileGrid[x, y] != 0)
+                return false;
+
+            float xOffset = (y % 2) * _xModY;
+            float yOffset = (x % 2) * _yModX;
+            float xPos = x * _xIncrement + xOffset;
+            float yPos = y * _yIncrement + yOffset;
+
+            var tile = Instantiate(tilePrefab);
+            tile.transform.position = new Vector2(xPos, yPos);
+            tile.GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(yPos * 1f * 100f);
+
+            return true;
+        }
+
+        private void ResetGrid()
+        {
+            for (int i = 0; i < _gridWidth; i++)
+                for (int k = 0; k < _gridHeight; k++)
+                    _tileGrid[i, k] = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Tile/Old Reference/TilePlacer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tile.Old
+{
+    public class TilePlacer : MonoBehaviour
+    {
+        private float _xIncrement = 1f;
+        private float _yIncrement = 0.25f;
+
+        [SerializeField]
+        private GameObject _tileObj;
+        [SerializeField]
+        private GameObject _tileObj2;
+
+        private TileMap _tileMap;
+
+        // Use this for initialization
+        void Start()
+        {
+            _tileMap = GetComponent<TileMap>();
+            _tileMap.PopulateGrid(_tileObj2);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //HandleMouse();
+        }
+
+        void HandleMouse()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mousePosition = Input.mousePosition;
+                var worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+                float xRaw = worldMousePosition.x;
+                float yRaw = worldMousePosition.y;
+
+                int xDist = Mathf.RoundToInt(xRaw / _xIncrement);
+                int yDist = Mathf.RoundToInt(yRaw / _yIncrement);
+
+                float xPos = xDist * _xIncrement;
+                float yPos = yDist * _yIncrement;
+
+                Debug.Log(xDist);
+                Debug.Log(yDist);
+
+                var tileGameObj = Instantiate(_tileObj);
+                tileGameObj.transform.position = new Vector2(xPos, yPos);
+
+                int sortOrder = Mathf.RoundToInt((yPos * -1) * 100);
+                tileGameObj.GetComponent<SpriteRenderer>().sortingOrder = sortOrder;
+            }
+        }
+    }
+}
